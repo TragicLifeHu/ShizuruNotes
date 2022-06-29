6d6c496f4b6aec571a2371b09ccc19c559ce58c5
Nyan Fantasia <nyanfantasia@gmail.com>
2022-06-29 16:05:17 +0800

    Kotlin migration

--- app/src/main/java/com/github/nyanfantasia/shizurunotes/db/RawUnitBasic.kt
+++ app/src/main/java/com/github/nyanfantasia/shizurunotes/db/RawUnitBasic.kt
@@ -1,101 +1,93 @@
-package com.github.nyanfantasia.shizurunotes.db;
+package com.github.nyanfantasia.shizurunotes.db
 
-import com.github.nyanfantasia.shizurunotes.R;
-import com.github.nyanfantasia.shizurunotes.common.Statics;
-import com.github.nyanfantasia.shizurunotes.data.Chara;
+import com.github.nyanfantasia.shizurunotes.R
+import com.github.nyanfantasia.shizurunotes.common.Statics
+import com.github.nyanfantasia.shizurunotes.data.Chara
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
-public class RawUnitBasic {
-    public int unit_id;
-    public int unit_conversion_id;
-    public String unit_name;
-    public int prefab_id;
-    public int move_speed;
-    public int search_area_width;
-    public int atk_type;
-    public double normal_atk_cast_time;
-    public int guild_id;
-    public String comment;
-    public String start_time;
-    public String age;
-    public String guild;
-    public String race;
-    public String height;
-    public String weight;
-    public String birth_month;
-    public String birth_day;
-    public String blood_type;
-    public String favorite;
-    public String voice;
-    public String catch_copy;
-    public String self_text;
-    public String actual_name;
-    public String kana;
-
-
-    public void setCharaBasic(Chara chara){
-        chara.setCharaId(unit_id / 100);
-
-        chara.setUnitId(unit_id);
-        chara.setUnitConversionId(unit_conversion_id);
-        chara.unitName = unit_name;
-        chara.setPrefabId(prefab_id);
-        chara.setSearchAreaWidth(search_area_width);
-        chara.setAtkType(atk_type);
-
-        chara.setMoveSpeed(move_speed);
-        chara.setNormalAtkCastTime(normal_atk_cast_time);
-        chara.actualName = actual_name;
-        chara.age = age;
-        chara.setGuildId(guild_id);
-
-        chara.guild = guild;
-        chara.race = race;
-        chara.height = height;
-        chara.weight = weight;
-        chara.birthMonth = birth_month;
-
-        chara.birthDay = birth_day;
-        chara.bloodType = blood_type;
-        chara.favorite = favorite;
-        chara.voice = voice;
-        chara.catchCopy = catch_copy;
-
-        chara.setComment(comment);
-        chara.kana = kana;
-        chara.setSelfText(self_text == null ? "" : self_text.replaceAll("\\\\n", "\n"));
+@Suppress("PropertyName")
+class RawUnitBasic {
+    var unit_id = 0
+    var unit_conversion_id = 0
+    var unit_name: String? = null
+    var prefab_id = 0
+    var move_speed = 0
+    var search_area_width = 0
+    var atk_type = 0
+    var normal_atk_cast_time = 0.0
+    var guild_id = 0
+    var comment: String? = null
+    var start_time: String? = null
+    var age: String? = null
+    var guild: String? = null
+    var race: String? = null
+    var height: String? = null
+    var weight: String? = null
+    var birth_month: String? = null
+    var birth_day: String? = null
+    var blood_type: String? = null
+    var favorite: String? = null
+    var voice: String? = null
+    var catch_copy: String? = null
+    var self_text: String? = null
+    var actual_name: String? = null
+    var kana: String? = null
+    fun setCharaBasic(chara: Chara) {
+        chara.charaId = unit_id / 100
+        chara.unitId = unit_id
+        chara.unitConversionId = unit_conversion_id
+        chara.unitName = unit_name!!
+        chara.prefabId = prefab_id
+        chara.searchAreaWidth = search_area_width
+        chara.atkType = atk_type
+        chara.moveSpeed = move_speed
+        chara.normalAtkCastTime = normal_atk_cast_time
+        chara.actualName = actual_name!!
+        chara.age = age!!
+        chara.guildId = guild_id
+        chara.guild = guild!!
+        chara.race = race!!
+        chara.height = height!!
+        chara.weight = weight!!
+        chara.birthMonth = birth_month!!
+        chara.birthDay = birth_day!!
+        chara.bloodType = blood_type!!
+        chara.favorite = favorite!!
+        chara.voice = voice!!
+        chara.catchCopy = catch_copy!!
+        chara.comment = comment
+        chara.kana = kana!!
+        chara.selfText = if (self_text == null) "" else self_text!!.replace("\\\\n".toRegex(), "\n")
 
         //需要处理的字串
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d H:m[:s]");
-        chara.startTime = LocalDateTime.parse(start_time, formatter);
-        chara.iconUrl = String.format(Locale.US, Statics.ICON_URL, prefab_id + 30);
-        chara.imageUrl = String.format(Locale.US, Statics.IMAGE_URL, prefab_id + 30);
+        val formatter = DateTimeFormatter.ofPattern("yyyy/M/d H:m[:s]")
+        chara.startTime = LocalDateTime.parse(start_time, formatter)
+        chara.iconUrl = String.format(Locale.US, Statics.ICON_URL, prefab_id + 30)
+        chara.imageUrl = String.format(Locale.US, Statics.IMAGE_URL, prefab_id + 30)
 
         // The correct age of Kasumi should be 15
-        if (unit_name.startsWith("カスミ") && voice.equals("水瀬いのり")) {
-            chara.age = "15";
+        if (unit_name!!.startsWith("カスミ") && voice == "水瀬いのり") {
+            chara.age = "15"
         }
         // The correct age of Monika should be 17
-        if (unit_name.startsWith("モニカ") && voice.equals("辻あゆみ")) {
-            chara.age = "17";
+        if (unit_name!!.startsWith("モニカ") && voice == "辻あゆみ") {
+            chara.age = "17"
         }
         // 109301: ルゥ
         if (unit_id == 109301) {
-            chara.age = "16";
+            chara.age = "16"
         }
-
-        if(search_area_width < 300) {
-            chara.position = "1";
-            chara.setPositionIcon(R.drawable.position_forward);
-        } else if(search_area_width < 600){
-            chara.position = "2";
-            chara.setPositionIcon(R.drawable.position_middle);
+        if (search_area_width < 300) {
+            chara.position = "1"
+            chara.positionIcon = R.drawable.position_forward
+        } else if (search_area_width < 600) {
+            chara.position = "2"
+            chara.positionIcon = R.drawable.position_middle
         } else {
-            chara.position = "3";
-            chara.setPositionIcon(R.drawable.position_rear);
+            chara.position = "3"
+            chara.positionIcon = R.drawable.position_rear
         }
     }
 }
