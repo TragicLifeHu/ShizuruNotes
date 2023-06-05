4db2e527e9bdec454174ce0bd64eb8b9b81c90ae
Nyan Fantasia <nyanfantasia@gmail.com>
2023-06-05 17:39:43 +0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0-alpha02")
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
@@ -64,7 +64,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation("com.google.guava:guava:32.0.0-jre")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     implementation("com.jakewharton:process-phoenix:2.1.2")
